d732228e8471fe5698a23501da6919b63ac339ae
leadstar116 <supremedev116@gmail.com>
2021-09-23 05:07:32 +1000

    change nft synthetic work

--- src/components/PriviDigitalArt/subpages/NFTFractionalisation/modals/ChangeLockedNFT/index.styles.tsx
+++ src/components/PriviDigitalArt/subpages/NFTFractionalisation/modals/ChangeLockedNFT/index.styles.tsx
@@ -4,16 +4,32 @@ export const ChangeLockedNFTStyles = makeStyles(theme => ({
   root: {
     maxWidth: "700px !important",
   },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "86px 38px 13px",
+
+    [theme.breakpoints.down('xs')]: {
+      padding: "60px 18px",
+    },
+  },
   title: {
     fontSize: 20,
     fontWeight: 800,
     lineHeight: "104.5%",
     color: "#181818",
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 18,
+    },
   },
   description: {
     fontSize: 16,
     lineHeight: "150%",
     color: "rgba(24, 24, 24, 0.7)",
+    [theme.breakpoints.down('xs')]: {
+      textAlign: 'center',
+    },
   },
   icon: {
     width: 160,

12b6875e2b5385844bdb808863f8817a246ac107
davidli108 <david.li.dev211@gmail.com>
2021-09-27 14:29:12 -0400

    Updated balance in collection card

--- src/components/PriviDigitalArt/components/Cards/SyntheticCollectionCard/index.tsx
+++ src/components/PriviDigitalArt/components/Cards/SyntheticCollectionCard/index.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import cls from "classnames";
+import Web3 from "web3";
+import { useWeb3React } from "@web3-react/core";
 import { useHistory } from "react-router-dom";
+import { BlockchainNets } from "shared/constants/constants";
+import { switchNetwork } from "shared/functions/metamask";
+import { useAlertMessage } from "shared/hooks/useAlertMessage";
 
 import { Divider } from "@material-ui/core";
 
@@ -10,6 +15,45 @@ import { syntheticCollectionCardStyles } from "./index.styles";
 export default function SyntheticCollectionCard({ item }) {
   const classes = syntheticCollectionCardStyles();
   const history = useHistory();
+  const { account, library, chainId } = useWeb3React();
+  const { showAlertMessage } = useAlertMessage();
+  const [balance, setBalance] = useState<number>(0);
+  const [price, setPrice] = useState<number>(0);
+
+  useEffect(() => {
+    (async () => {
+      const targetChain = BlockchainNets[1];
+
+      if (chainId && chainId !== targetChain?.chainId) {
+        const isHere = await switchNetwork(targetChain?.chainId || 0);
+        if (!isHere) {
+          showAlertMessage(`Got failed while switching over to polygon network`, { variant: "error" });
+          return;
+        }
+      }
+
+      const web3APIHandler = targetChain.apiHandler;
+      const web3Config = targetChain.config;
+      const web3 = new Web3(library.provider);
+
+      const balanceResponse = await web3APIHandler.SyntheticCollectionManager.getContractJotsBalance(
+        web3,
+        item
+      );
+
+      if (balanceResponse) {
+        setBalance(balanceResponse);
+      }
+
+      const priceResponse = await web3APIHandler.SyntheticCollectionManager.getJotFractionPrice(web3, item, {
+        tokenId: +item.SyntheticID,
+      });
+
+      if (priceResponse) {
+        setPrice(priceResponse);
+      }
+    })();
+  }, []);
 
   return (
     <div
@@ -35,11 +79,11 @@ export default function SyntheticCollectionCard({ item }) {
             </Box>
             <Box className={classes.detailWrapper}>
               <Box className={classes.detailLabel}>Fraction Price</Box>
-              <Box className={classes.detailInfo}>2,44 USDC</Box>
+              <Box className={classes.detailInfo}>{price} USDC</Box>
             </Box>
             <Box className={classes.detailWrapper}>
               <Box className={classes.detailLabel}>Implied Valuation</Box>
-              <Box className={classes.detailInfo}>$2,543,437</Box>
+              <Box className={classes.detailInfo}>${balance}</Box>
             </Box>
           </Box>
         </Box>

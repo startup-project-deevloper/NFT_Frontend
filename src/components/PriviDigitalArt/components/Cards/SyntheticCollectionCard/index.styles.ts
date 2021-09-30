c015b9e610f2a3beb6ad3c12993ad350059c8c12
nazar <nazar.mych123@gmail.com>
2021-09-30 23:10:46 +0800

    fixed carousel issue

--- src/components/PriviDigitalArt/components/Cards/SyntheticCollectionCard/index.styles.ts
+++ src/components/PriviDigitalArt/components/Cards/SyntheticCollectionCard/index.styles.ts
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core";
 export const syntheticCollectionCardStyles = makeStyles(theme => ({
   card: {
     minWidth: 276,
+    maxWidth: 370,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center',

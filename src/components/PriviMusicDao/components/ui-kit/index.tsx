1406eac069bd88564b23f8fd5c18b8494733bc5a
nmorga <nicolasmorga514@gmail.com>
2021-10-06 22:24:04 +0200

    Remove Get Privi Wallet button

--- src/components/PriviMusicDao/components/ui-kit/index.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { Color, Text as StyledText, TextProps } from "shared/ui-kit";
-
-export const Text = styled(StyledText)<TextProps>`
-  color: ${p => p.color || Color.MusicDAODark};
-`;

f6cfbc01d288660d626d84a8358d26cb916bfc74
davidli108 <david.li.dev211@gmail.com>
2021-10-18 10:48:48 -0400

    Fixed url for pod detail page

--- src/components/PriviDigitalArt/components/Cards/PodProposalCard/index.tsx
+++ src/components/PriviDigitalArt/components/Cards/PodProposalCard/index.tsx
@@ -200,7 +200,7 @@ export default function PodProposalCard({ pod }) {
             size="medium"
             style={{ background: Color.Purple, padding: "0 20px", fontSize: '14px', fontWeight: 600 }}
             isRounded
-            onClick={() => history.push(`/pods/${podData.Id}`)}
+            onClick={() => history.push(`/pods/${podData.id}`)}
           >
             OPEN POD
           </PrimaryButton>

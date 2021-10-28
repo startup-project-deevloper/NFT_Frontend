b2dfc37fd92c32d39631f33b47dc7a1a8b3a50d0
iprodev211 <greg.li.dev@gmail.com>
2021-10-28 11:48:41 -0400

    Fixed text of jot price

--- src/components/PriviDigitalArt/subpages/NFTFractionalisation/components/SyntheticFractionalisedCollectionPage/index.tsx
+++ src/components/PriviDigitalArt/subpages/NFTFractionalisation/components/SyntheticFractionalisedCollectionPage/index.tsx
@@ -528,7 +528,7 @@ const SyntheticFractionalisedCollectionPage = ({ goBack, match }) => {
               <div className={classes.typo6}>ACCRUED REWARD</div>
             </Box>
             <Box display="flex" flexDirection="column">
-              <div className={classes.typo3}>{jotPrice} </div>
+              <div className={classes.typo3}>{jotPrice || 'N/A'} </div>
               <div className={classes.typo4}>JOT PRICE</div>
             </Box>
             {(isMobile || isTablet) && (

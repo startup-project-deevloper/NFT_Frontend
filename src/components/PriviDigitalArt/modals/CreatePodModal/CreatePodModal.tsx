51c5cb22b268ec079b0a8ef995c1e3da4ad32ad5
volodymyrboian <volodmyr@protonmail.com>
2021-11-01 04:05:29 +0800

    fix small ui issues on pod detail

--- src/components/PriviDigitalArt/modals/CreatePodModal/CreatePodModal.tsx
+++ src/components/PriviDigitalArt/modals/CreatePodModal/CreatePodModal.tsx
@@ -113,10 +113,10 @@ const CreatePodModal = (props: any) => {
 
   const validateNFTMediaInfoCreate = () => {
     if (pod.Name.length <= 5) {
-      showAlertMessage(`Name field invalid. Minimum 5 characters required`, { variant: "error" });
+      showAlertMessage(`Name field invalid.`, { variant: "error" });
       return false;
     } else if (pod.Description.length <= 20) {
-      showAlertMessage(`Description field invalid. Minimum 20 characters required`, { variant: "error" });
+      showAlertMessage(`Description field invalid.`, { variant: "error" });
       return false;
     } else return true;
   };

39c5d5c5a0bb3ce11086874a21c79da7c653ed14
nicolas <nclszhang92@gmail.com>
2021-11-12 10:58:47 -0700

    fix style issue in pix message item

--- src/shared/ui-kit/Header/components/Message/ListItem.tsx
+++ src/shared/ui-kit/Header/components/Message/ListItem.tsx
@@ -55,6 +55,8 @@ export const ListItem: React.FunctionComponent<ListItemProps> = ({ chat, handleC
                 <img src={require("assets/icons/music-solid.svg")} alt={"music-solid"} />
                 &nbsp;Audio
               </div>
+            ) : chat.lastMessage.length > 75 ? (
+              `${chat.lastMessage.substring(0, 75)} ...`
             ) : (
               chat.lastMessage.substring(0, 75)
             )}

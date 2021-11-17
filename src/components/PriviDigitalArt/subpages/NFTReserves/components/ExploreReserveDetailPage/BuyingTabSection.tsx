74752d7ff3c0fb46030a4f94341cf08d1dc469af
davidli108 <david.li.dev211@gmail.com>
2021-11-17 15:55:22 -0500

    Added buying tab

---
+++ src/components/PriviDigitalArt/subpages/NFTReserves/components/ExploreReserveDetailPage/BuyingTabSection.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+
+import { PrimaryButton } from "shared/ui-kit";
+import Box from "shared/ui-kit/Box";
+import { CustomTable, CustomTableHeaderInfo } from "shared/ui-kit/Table";
+import { exploreOptionDetailPageStyles } from "./index.styles";
+import { TagIcon, HistoryIcon } from "./index";
+
+export default ({ offerData, historyData }) => {
+  const classes = exploreOptionDetailPageStyles();
+  const offerTableHeaders: Array<CustomTableHeaderInfo> = [
+    {
+      headerName: "USER",
+    },
+    {
+      headerName: "PRICE",
+      headerAlign: "center",
+    },
+    {
+      headerName: "EXPIRATION DATE",
+      headerAlign: "center",
+    },
+    {
+      headerName: "ETHERSCAN",
+      headerAlign: "center",
+    },
+  ];
+  const historyTableHeaders: Array<CustomTableHeaderInfo> = [
+    {
+      headerName: "USER",
+    },
+    {
+      headerName: "PRICE PER SECOND",
+      headerAlign: "center",
+    },
+    {
+      headerName: "ESTIMATED COST",
+      headerAlign: "center",
+    },
+    {
+      headerName: "RENTAL TIME",
+      headerAlign: "center",
+    },
+    {
+      headerName: "DATE",
+      headerAlign: "center",
+    },
+    {
+      headerName: "ETHERSCAN",
+      headerAlign: "center",
+    },
+  ];
+
+  return (
+    <>
+      <div className={classes.transactionsSection}>
+        <div className={classes.coinFlipHistorySection}>
+            <Box display="flex" alignItems="center" justifyContent="space-between" m="36px 30px 0 50px">
+              <div className={classes.typo8}><TagIcon /><span>Buy offers</span></div>
+              <PrimaryButton
+                size="small"
+                className={classes.pricingButton}
+              >
+                MAKE BUY OFFER
+              </PrimaryButton>
+            </Box>
+            <div className={classes.table}>
+                <CustomTable
+                  headers={offerTableHeaders}
+                  rows={offerData.map(item => [
+                    {
+                      cell: item.user,
+                    },
+                    {
+                      cell: item.price,
+                    },
+                    {
+                      cell: item.date
+                    },
+                    {
+                      cellAlign: "center",
+                      cell: (
+                        <div>
+                        <img src={require("assets/icons/icon_ethscan.png")} />
+                        </div>
+                      ),
+                    },
+                  ])}
+                  placeholderText="No history"
+                />
+            </div>
+        </div>
+      </div>
+      <div className={classes.transactionsSection}>
+        <div className={classes.coinFlipHistorySection}>
+            <Box display="flex" alignItems="center" justifyContent="space-between" m="36px 30px 0 50px">
+              <div className={classes.typo8}><HistoryIcon /><span>Buy History</span></div>
+            </Box>
+            <div className={classes.table}>
+                <CustomTable
+                  headers={historyTableHeaders}
+                  rows={historyData.map(item => [
+                    {
+                      cell: item.user,
+                    },
+                    {
+                      cell: item.price,
+                    },
+                    {
+                      cell: item.estimated,
+                    },
+                    {
+                      cell: item.time
+                    },
+                    {
+                      cell: item.date
+                    },
+                    {
+                      cellAlign: "center",
+                      cell: (
+                        <div>
+                        <img src={require("assets/icons/icon_ethscan.png")} />
+                        </div>
+                      ),
+                    },
+                  ])}
+                  placeholderText="No history"
+                />
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
